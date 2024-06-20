b3f45ec474aab823935efd66814dca195d9bd51d
AhmedAymanSalah <aa3901@fayoum.edu.eg>
2024-06-20 21:12:32 +0300

    chat done

--- lib/chat/ui/screens/chatPage.dart
+++ lib/chat/ui/screens/chatPage.dart
@@ -1,22 +1,21 @@
-import 'package:campuspay/chat/model/message.dart';
 import 'package:campuspay/chat/ui/widget/chatBuble.dart';
-import 'package:campuspay/core/theme/colors.dart';
 import 'package:campuspay/core/utils/constant.dart';
-import 'package:campuspay/core/widgets/custom_text_widget.dart';
-import 'package:campuspay/features/profile/data/presentation/manage/cubit/cubit.dart';
-import 'package:campuspay/features/profile/data/presentation/manage/cubit/states.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';import 'package:campuspay/core/widgets/custom_text_widget.dart';
+import 'package:campuspay/chat/model/message.dart';
+import 'package:campuspay/core/theme/colors.dart';
+import 'package:campuspay/features/profile/data/presentation/manage/cubit/cubit.dart';
+import 'package:campuspay/features/profile/data/presentation/manage/cubit/states.dart';
 
 class ChatPage extends StatelessWidget {
-  static String id = 'ChatPage';
+  static const String id = 'ChatPage';
 
-  final _controller = ScrollController();
-  final TextEditingController controller = TextEditingController();
+  final ScrollController _controller = ScrollController();
+  final TextEditingController _textController = TextEditingController();
 
-  final CollectionReference messages = 
-    FirebaseFirestore.instance.collection(kMessagesCollections);
+  final CollectionReference messages =
+      FirebaseFirestore.instance.collection('messages');
 
   ChatPage({super.key});
 
@@ -25,101 +24,111 @@ class ChatPage extends StatelessWidget {
     var email = ModalRoute.of(context)!.settings.arguments as String?;
 
     return BlocProvider(
-      create: (BuildContext context)=>ProfileCubit()..getProfile(),
-      child: BlocConsumer<ProfileCubit,ProfileStates>(
+      create: (BuildContext context) => ProfileCubit()..getProfile(),
+      child: BlocConsumer<ProfileCubit, ProfileStates>(
         listener: (BuildContext context, state) {},
         builder: (BuildContext context, Object? state) {
-          var cubit=ProfileCubit().get(context);
-          if(cubit.profileModel !=null) {
+          var cubit = ProfileCubit().get(context);
+          if (cubit.profileModel != null) {
             return StreamBuilder<QuerySnapshot>(
-      stream: messages.orderBy(kCreatedAt, descending: true).snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          List<Message> messagesList = snapshot.data!.docs.map((doc) {
-            return Message.fromJson(doc);
-          }).toList();
+              stream:
+                  messages.orderBy('createdAt', descending: true).snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<Message> messagesList = snapshot.data!.docs
+                      .map((doc) => Message.fromJson(doc))
+                      .toList();
 
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: ColorsManager.darkBlue,
-              centerTitle: true,
-              title: const CustomTextWidget(
-                text: "Chat",
-                color: ColorsManager.white,
-              ),
-            ),
-            body: Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    reverse: true,
-                    controller: _controller,
-                    itemCount: messagesList.length,
-                    itemBuilder: (context, index) {
-                      return messagesList[index].id == email
-                          ? ChatBuble(message: messagesList[index])
-                          : ChatBubleForFriend(message: messagesList[index]);
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: controller,
-                          decoration: InputDecoration(
-                            hintText: 'Message',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16),
-                              borderSide: const BorderSide(
-                                color: kPrimaryColor,
-                              ),
-                            ),
+                  return Scaffold(
+                    appBar: AppBar(
+                      backgroundColor: ColorsManager.darkBlue,
+                      centerTitle: true,
+                      title: const CustomTextWidget(
+                        text: "Chat",
+                        color: ColorsManager.white,
+                      ),
+                    ),
+                    body: Column(
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            reverse: true,
+                            controller: _controller,
+                            itemCount: messagesList.length,
+                            itemBuilder: (context, index) {
+                              // Determine if the message is from the current user or other users
+                              bool isUserMessage =
+                                  messagesList[index].id == cubit.profileModel!.email!;
+
+                              return Align(
+                                alignment: isUserMessage
+                                    ? Alignment.centerRight
+                                    : Alignment.centerLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 8, horizontal: 16),
+                                  child: ChatBubble(
+                                    message: messagesList[index],
+                                    isUserMessage: isUserMessage,
+                                  ),
+                                ),
+                              );
+                            },
                           ),
                         ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.send, color: kPrimaryColor),
-                        onPressed: () {
-                          if (controller.text.isNotEmpty) {
-                            messages.add({
-                              kMessage: controller.text,
-                              kCreatedAt: DateTime.now(),
-                              'id': email,
-                            });
-                            controller.clear();
-                            _controller.animateTo(
-                              0,
-                              duration: const Duration(milliseconds: 500),
-                              curve: Curves.easeIn,
-                            );
-                          }
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          );
-        } else {
-          return const Scaffold(
-            body: Center(
-              child: Text(
-                'Loading ...',
-                style: TextStyle(
-                  color: Colors.amberAccent,
-                  fontSize: 30,
-                ),
-              ),
-            ),
-          );
-        }
-      },
-    );
-          }else{
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: _textController,
+                                  decoration: InputDecoration(
+                                    hintText: 'Message',
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(16),
+                                      borderSide: const BorderSide(
+                                        color: kPrimaryColor,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.send, color: kPrimaryColor),
+                                onPressed: () {
+                                  if (_textController.text.isNotEmpty) {
+                                    messages.add({
+                                      'message': _textController.text,
+                                      'createdAt': DateTime.now(),
+                                      'id': cubit.profileModel!.email!,
+                                    });
+                                    _textController.clear();
+                                    _controller.animateTo(
+                                      0,
+                                      duration:
+                                          const Duration(milliseconds: 500),
+                                      curve: Curves.easeIn,
+                                    );
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                } else {
+                  return const Scaffold(
+                    body: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+              },
+            );
+          } else {
             return const Center(child: CircularProgressIndicator());
           }
         },
--- lib/chat/ui/widget/chatBuble.dart
+++ lib/chat/ui/widget/chatBuble.dart
@@ -1,101 +1,77 @@
-
-
 import 'package:campuspay/chat/model/message.dart';
-import 'package:campuspay/core/utils/constant.dart';
+import 'package:campuspay/core/helpers/spacing.dart';
+import 'package:campuspay/core/widgets/custom_text_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ChatBuble extends StatelessWidget {
-  const ChatBuble({
+import 'package:intl/intl.dart';
+
+import 'package:campuspay/core/theme/colors.dart';
+
+class ChatBubble extends StatelessWidget {
+  final Message message;
+  final bool isUserMessage;
+
+  const ChatBubble({
     Key? key,
     required this.message,
+    required this.isUserMessage,
   }) : super(key: key);
-  
-  final Message message;
 
   @override
   Widget build(BuildContext context) {
     String formattedTime = DateFormat('hh:mm a').format(message.createdAt);
 
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Container(
-        padding: const EdgeInsets.only(left: 16, top: 16, bottom: 16, right: 16),
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(32),
-            topRight: Radius.circular(32),
-            bottomRight: Radius.circular(32),
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: isUserMessage ? ColorsManager.darkBlue : Colors.grey[300],
           ),
-          color: kPrimaryColor,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(message.content),
-            SizedBox(height: 5),
-            Text(
-              formattedTime,
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 12,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTextWidget(
+                text: message.id ?? 'Unknown',
+                fontSize: 13.sp,
+                fontWeight: FontWeight.bold,
+                color: isUserMessage ? Colors.white70 : Colors.black54,
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-
-class ChatBubleForFriend extends StatelessWidget {
-  const ChatBubleForFriend({
-    super.key,
-    required this.message,
-  });
-  
-  final Message message;
-
-  @override
-  Widget build(BuildContext context) {
-    String formattedTime = DateFormat('hh:mm a').format(message.createdAt);
-
-    return Align(
-      alignment: Alignment.centerRight,
-      child: Container(
-        padding: const EdgeInsets.only(left: 16, top: 16, bottom: 16, right: 16),
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(32),
-            topRight: Radius.circular(32),
-            bottomLeft: Radius.circular(32),
+              verticalSpace(2),
+              CustomTextWidget(
+                text: message.content,
+                color: isUserMessage ? Colors.white : Colors.black,
+                fontSize: 14,
+                maxLines: 100,
+                fontWeight: FontWeight.bold,
+              ),
+            ],
           ),
-          color: Color(0xff006D84),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              message.content,
-              style: const TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            SizedBox(height: 5),
-            Text(
-              formattedTime,
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 12,
-              ),
-            ),
-          ],
+        verticalSpace(15),
+        Positioned(
+          bottom: 0,
+          right: 12,
+          // child: Container(
+          //   padding: EdgeInsets.symmetric(horizontal: 8, vertical: 1),
+          //   decoration: BoxDecoration(
+          //     color: isUserMessage ? Colors.blueGrey : Colors.grey[300],
+          //     borderRadius: BorderRadius.only(
+          //       topLeft: Radius.circular(10),
+          //       bottomRight: Radius.circular(20),
+          //     ),
+          //   ),
+          child: CustomTextWidget(
+            text: formattedTime,
+            fontSize: 13.sp,
+            color: isUserMessage ? Colors.white70 : Colors.black54,
+          ),
         ),
-      ),
+        // ),
+      ],
     );
   }
 }
